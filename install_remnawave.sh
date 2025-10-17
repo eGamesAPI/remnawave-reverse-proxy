ea94dbaf2c2c402c052f3ed87937efc0c29707a1
Ivan Kolesnikov <b1svixyz@mail.ru>
2025-10-17 22:07:15 +0700

    fix: typo

--- install_remnawave.sh
+++ install_remnawave.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_VERSION="2.1.10af"
+SCRIPT_VERSION="2.1.10a"
 UPDATE_AVAILABLE=false
 DIR_REMNAWAVE="/usr/local/remnawave_reverse/"
 LANG_FILE="${DIR_REMNAWAVE}selected_language"
