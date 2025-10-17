795a71ab31a4edf4cbf3f7c90fc7df0454af6fa2
Ivan Kolesnikov <b1svixyz@mail.ru>
2025-10-17 21:44:13 +0700

    bump: setting the right version

--- install_remnawave.sh
+++ install_remnawave.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SCRIPT_VERSION="2.1.10"
+SCRIPT_VERSION="2.1.10af"
 UPDATE_AVAILABLE=false
 DIR_REMNAWAVE="/usr/local/remnawave_reverse/"
 LANG_FILE="${DIR_REMNAWAVE}selected_language"
