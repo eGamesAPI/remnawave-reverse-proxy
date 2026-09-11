0651380dd92bd0070c5746a94da03a0f1c9ea9e0
eGames <33666852+eGamesAPI@users.noreply.github.com>
2026-09-12 00:52:56 +0500

    Update install_remnawave.sh

--- install_remnawave.sh
+++ install_remnawave.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_VERSION="Dev 3.2.7"
+SCRIPT_VERSION="DEv 3.2.7"
 UPDATE_AVAILABLE=false
 DIR_REMNAWAVE="/usr/local/remnawave_reverse/"
 LANG_FILE="${DIR_REMNAWAVE}selected_language"
