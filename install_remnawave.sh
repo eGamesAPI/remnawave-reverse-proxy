dcd3656a1ae2af97e79439775512401bd9233d2c
eGames <pm@egam.es>
2026-09-21 14:29:04 +0500

    bump version to Dev 3.5.65 for the cert report grouping

--- install_remnawave.sh
+++ install_remnawave.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_VERSION="Dev 3.5.64"
+SCRIPT_VERSION="Dev 3.5.65"
 UPDATE_AVAILABLE=false
 DIR_REMNAWAVE="/usr/local/remnawave_reverse/"
 LANG_FILE="${DIR_REMNAWAVE}selected_language"
