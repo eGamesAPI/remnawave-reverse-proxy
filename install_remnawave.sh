c03a217666e43eee30fb4f3e8e2dfdb7b0638d2e
eGames <pm@egam.es>
2026-09-21 14:11:06 +0500

    bump version to Dev 3.5.63 for the DNS zone lookup fix

--- install_remnawave.sh
+++ install_remnawave.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_VERSION="Dev 3.5.62"
+SCRIPT_VERSION="Dev 3.5.63"
 UPDATE_AVAILABLE=false
 DIR_REMNAWAVE="/usr/local/remnawave_reverse/"
 LANG_FILE="${DIR_REMNAWAVE}selected_language"
